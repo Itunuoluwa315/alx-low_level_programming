c1334b24199ca1a65404afe180951b612a5b06fe
itunuoluwa315 <olatunjisamuel315@gmail.com>
2022-12-09 18:19:08 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - Entry Point
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like buiding a multilingual puzzle");
 	return (0);
 
-}	
+}
