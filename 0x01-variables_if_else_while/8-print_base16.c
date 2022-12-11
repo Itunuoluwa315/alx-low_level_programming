5f5bafb1c0b70f8d48271243ddb5b8600aba32dd
itunuoluwa315 <olatunjisamuel315@gmail.com>
2022-12-11 08:36:00 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int num = '0'; /*Initialize num to 0*/
-	char letter = 'a'; /*Initialize letter to a */
+	char letter = 'a'; /*Initialize letter to a*/
 
 	while (num <= '9') /*Print 0-9*/
 	{
@@ -24,7 +24,7 @@ int main(void)
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 
 }
