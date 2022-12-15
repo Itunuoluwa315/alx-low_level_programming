07b007238d7272dcb00b6ff0c6d3916b7a39a24f
itunuoluwa315 <olatunjisamuel315@gmail.com>
2022-12-15 04:03:01 -0800

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,5 +7,6 @@ void postive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int da, int year);
 int convert_day(int month, int day);
+void print_times_table(int n);
 
-#endif /* MAIN_H */
+#endif
