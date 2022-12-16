8c77fa0ff3c674b19c1b48413e34fe63810c9775
itunuoluwa315 <olatunjisamuel315@gmail.com>
2022-12-16 12:08:16 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests functions that prints postive or negative integer
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive or negative(i);
+
+	return (0)
+}
